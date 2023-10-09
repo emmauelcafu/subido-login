ae98a6f3aae0143548de1ccf648413ea3b513894
emmanuelcafu <emmauelcafu@gmail.com>
2023-10-09 14:26:17 -0500

    se actualiza

--- api/src/db.js
+++ api/src/db.js
@@ -1,12 +1,12 @@
 const {Sequelize} = require("sequelize");
 require("dotenv").config();
 const{
-    DB_USER,DB_PASSWORD,DB_HOST
+    DB_USER,DB_PASSWORD,DB_HOST,DB_NAME
 }= process.env;
 
 
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/login`,{
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,{
     logging: false, // set to console.log to see the raw SQL queries
   native: false,
 });
